116b78d3ea0201d69389ea11486c75984c99a27e
savelevaleksandr <dvikt3@gmail.com>
2022-05-20 22:36:12 +0300

    solution

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(unit "/Users/savelevaleksandr/CLionProjects/bonus2/cmake-build-debug/bin/tests")
-set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/Users/savelevaleksandr/CLionProjects/bonus2/CMakeLists.txt;18;add_test;/Users/savelevaleksandr/CLionProjects/bonus2/CMakeLists.txt;0;")
+set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/Users/savelevaleksandr/CLionProjects/bonus2/CMakeLists.txt;22;add_test;/Users/savelevaleksandr/CLionProjects/bonus2/CMakeLists.txt;0;")
 subdirs("googletest")
--- solution.h
+++ solution.h
@@ -4,14 +4,23 @@
 #include "fake_crypto.h"
 
 
-std::tuple<vector_t, vector_t, vector_t> FakeSign(const std::string&, RWEPublicKey) {
+std::tuple<vector_t, vector_t, vector_t> FakeSign(const std::string& string, RWEPublicKey publicKey) {
     vector_t c = {};
     vector_t z1 = {};
     vector_t z2 = {};
-
+    auto ase = publicKey.getASEVector();
+    auto a = publicKey.getAVector();
+    auto module = publicKey.getModule();
+    auto hash = ComputeSimpleHash(string);
     // your solution here;
 
-    throw std::runtime_error("Not implemented");
+    for(auto& i: c) {
+        i = 1;
+    }
+    for(auto& i : z1) {
+        i = 1;
+    }
+    z2 = c * ase + c - hash - a * z1;
 
     return std::make_tuple(c, z1, z2);
 }
